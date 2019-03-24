c0ba52375f611bed433bbed4be7cc837004f55a2
arham <arham_jain1108@outlook.com>
2019-03-25 01:59:28 +0530

    Completed GSP dashboard class, end-point, frontend and integration with existing functionalities

--- app.py
+++ app.py
@@ -3,9 +3,18 @@ from KPI_Automation import select_query, exec_query
 import pandas as pd
 from forms import Param_conn_select_form, Param_kpi_select_form
 from config import Config
+import matplotlib.pyplot as plt
+import numpy as np
+from Graph_Automation import GSP
+from time import sleep
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
+data = {
+    'kpi':0,
+    'conn':0,
+}
 
 '''
 Route pate = "/" "/home"
@@ -39,8 +48,12 @@ def param_selection():
     form_kpi.kpi.choices = exec_query("select distinct `Indicator Code`, `Indicator Name` from staging order by `Indicator Name` ASC")
     form_conn.conn.choices = exec_query("select distinct `Indicator Code`, `Indicator Name` from staging order by `Indicator Name` DESC")
     if form_kpi.validate_on_submit():
+        data['kpi'] = 1
+        data['conn'] = 0
         return redirect('/dashboard/{}/{}'.format(form_kpi.kpi.data, form_kpi.year.data))
     if form_conn.validate_on_submit():
+        data['kpi'] = 0
+        data['conn'] = 1
         return redirect('/dashboard/{}/{}'.format(form_conn.conn.data, form_conn.year.data))
     return render_template('basic/param_selection.html', form_kpi=form_kpi, form_conn=form_conn)
 
@@ -63,7 +76,49 @@ def kpi_eda(indicator_code,year):
         "kpi_year": year,
         "coun_kpi": zip(df['Country Name'].tolist(),df[year].tolist())
     }
+    if data['kpi']==1:
+        data['kpi_data'] = zip(df['Country Name'].tolist(),df[year].tolist())
+
+    if data['conn']==1:
+        data['conn_data'] = zip(df['Country Name'].tolist(),df[year].tolist())
+
+    ## NEEDS TO CHANGE
+    bp = plt.figure()
+    boxplot = df.boxplot(column=[year])
+    bp.savefig("static/images/eda/boxplot.svg", format="svg")
+
+    df['Country Code'] = np.arange(1,len(df)+1)
+    print(df)
+    scatterplot = df.plot.scatter(x = 'Country Code',y = year, c = 'DarkBlue')
+    sp = scatterplot.get_figure()
+    sp.savefig("static/images/eda/scatterplot.svg", format="svg")
+
     return render_template("basic/kpi_eda.html", data=dict_payload)
+
+'''
+Route path = "/dashboard/gspboard"
+
+This endpoint performs GSP based functions on the data submitted to param_selection. The data is accessed from the 
+'data' dictionary
+'''
+@app.route("/dashboard/gspboard")
+def dashboard():
+    gsp = GSP(data['kpi_data'], data['conn_data'])
+    print(gsp.conn_dict)
+    print(gsp.kpi_dict)
+    gsp.graph_signal()
+    gsp.gsp_plots()
+    return render_template("basic/gspboard.html")\
+
+'''
+Route path = "/dashboard/gspboard/new"
+
+To reload GSP page. Overcomes Caching issue.
+'''
+@app.route("/dashboard/gspboard/new")
+def dashboard_new():
+    return render_template("basic/gspboard.html")
+
 
 if __name__=="__main__":
     app.run(debug=True)
